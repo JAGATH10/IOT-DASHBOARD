8ba314e0c5f76244e70625c0d3790999f463ba3b
JAGATH_10 <106587271+JAGATH10@users.noreply.github.com>
2024-11-08 15:09:27 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://zmlsceuzzabvkppxzyku.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InptbHNjZXV6emFidmtwcHh6eWt1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MjU5NTA0MCwiZXhwIjoyMDA4MTcxMDQwfQ.X9ySlHUAHRRFYqftQhcNBzcuS6Qp3Dl5Qw-aJ6xpVD0";    
+const supabaseUrl = 'https://vyuzcsnrtdazonleimsr.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5dXpjc25ydGRhem9ubGVpbXNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA5Njg3MDMsImV4cCI6MjA0NjU0NDcwM30.7f2CDQfeaY_nwol3WxipLO71YABsBLsIdoWI8fBnT3g";    
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
